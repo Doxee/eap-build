1a723e250ef29769c0b728e6edc0c92a11787aaa
Alexis Hassler <alexis@sewatech.org>
2013-03-09 18:17:22 +0100

    Move for 6.1.0.Alpha1

--- build.sh
+++ build.sh
@@ -1,22 +1,31 @@
+# JBoss EAP 6.1.0.Alpha1
 rm -rf build
 mkdir build
 
-wget ftp://ftp.redhat.com/redhat/jbeap/6.0.1/en/source/jboss-eap-6.0.1-src.zip
-unzip -d build jboss-eap-6.0.1-src.zip
-rm jboss-eap-6.0.1-src.zip
+# Source code
+wget ftp://ftp.redhat.com/redhat/jbeap/6.1.0/en/source/jboss-eap-6.1.0.Alpha-src.zip
+unzip -d build jboss-eap-6.1.0.Alpha-src.zip
+rm jboss-eap-6.1.0.Alpha-src.zip
 
-wget http://maven.repository.redhat.com/techpreview/eap6/6.0.1/jboss-eap-6.0.1-maven-repository.zip
-unzip -d build jboss-eap-6.0.1-maven-repository.zip
-rm jboss-eap-6.0.1-maven-repository.zip
+# Maven repository
+wget http://maven.repository.redhat.com/techpreview/eap6/6.1.0.Alpha1/jboss-eap-6.1.0.Alpha-maven-repository.zip
+unzip -d build jboss-eap-6.1.0.Alpha-maven-repository.zip
+rm jboss-eap-6.1.0.Alpha-maven-repository.zip
 
-cd build/jboss-eap-6.0.1-maven-repository/
-patch -p 1 < ../../src/jboss-eap-6.0.1-maven-repository.patch
-cd ../..
-cp src/settings.xml build/jboss-eap-6.0-src/tools/maven/conf/
+# Patch (but nothing to patch with the alpha version)
+#cd build/jboss-eap-6.1.0.Alpha-maven-repository/
+#patch -p 1 < ../../src/jboss-eap-6.1.0.Alpha-maven-repository.patch
+#cd ../..
+#cp src/settings.xml build/jboss-eap-6.1.0.Alpha-src/tools/maven/conf/
 
-export EAP_REPO_URL=file://`pwd`/build/jboss-eap-6.0.1-maven-repository/
-cd build/jboss-eap-6.0-src/
-./build.sh
+# It's time to build
+export EAP_REPO_URL=file://`pwd`/build/jboss-eap-6.1.0.Alpha-maven-repository/
+cd build/jboss-eap-6.1-src/
+./build.sh -DskipTests -Drelease=true
 
-cp -R build/target/jboss-eap-6.0 ../
+# Save the result, throw away the other files
+mv dist/target/jboss-eap-6.1.* ../
+cd ..
+rm -rf jboss-eap-6.1-src
+rm -rf jboss-eap-6.1.0.Alpha1-maven-repository
 
